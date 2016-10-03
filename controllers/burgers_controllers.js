fb3aab8e27609ddc19bdcd009c7a37c5e327a42f
Antonio Montalvo <montalvocode@yahoo.com>
2016-10-03 11:33:47 -0700

    add controllers and views

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,12 @@
+/*
+Here is where you create all the functions that will do the routing for your app, and the logic of each route.
+*/
+var express = require('express');
+var router = express.Router();// I control the routes.
+var cat = require('../models/burgers.js');//access to the database
+
+
+
+
+
+module.exports = router;
